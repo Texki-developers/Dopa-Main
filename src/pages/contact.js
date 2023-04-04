e10b37ca1778e7399a269a3ae94ad299a585b686
rishadpt <rishadpt16@gmail.com>
2023-04-04 12:24:30 +0530

    contact us

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,16 +1,26 @@
-import Form from '@/Components/Form/Form'
-import MainLayout from '@/Layouts/MainLayout'
-import React from 'react'
+import Form from "@/Components/Form/Form";
+import MainLayout from "@/Layouts/MainLayout";
+import React from "react";
+import styles from "@/styles/contact.module.scss";
 
 export default function contact() {
+
   return (
     <MainLayout>
-    <div style={{display:'flex',justifyContent:'center',alignItems:'center',height:'100vh'}}>
-        <div style={{width:'30rem',display:'flex',alignItems:'center',flexDirection:'column',gap:'1rem'}}>
-    <h1 style={{color:'#1987a4',marginTop:'1.5rem'}}>Contact Us</h1>
-        <Form/>
+      <div className={styles.contact_container}>
+        <div
+          style={{
+            width: "30rem",
+            display: "flex",
+            alignItems: "center",
+            flexDirection: "column",
+            gap: "1rem",
+          }}
+        >
+          <h1 style={{ color: "#1987a4", marginTop: "1.5rem" }}>Contact Us</h1>
+          <Form />
         </div>
-    </div>
+      </div>
     </MainLayout>
-  )
+  );
 }
