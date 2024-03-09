c56bcf474659f22102cdaefde4708a6612a504df
Muhsin Neyyathur <muhsinny333@gmail.com>
2024-03-10 01:36:46 +0530

    feat: All sections except gallery is done

---
+++ src/PageComponents/DopaThrissurLanding/StudentTestimonials/StudentTestimonials.jsx
@@ -0,0 +1,85 @@
+import Center from "@/Components/BasicComponents/Center/Center";
+import HStack from "@/Components/BasicComponents/HStack/HStack";
+import VStack from "@/Components/BasicComponents/VStack/VStack";
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import SwiperCore, { Navigation } from "swiper/core";
+import { MdOutlineArrowForward } from "react-icons/md";
+
+SwiperCore.use([Navigation]);
+
+export default function StudentTestimonials() {
+  const swiperParams = {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    loop: true,
+    navigation: {
+      nextEl: ".testimonial-custom-next",
+      prevEl: ".testimonial-custom-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  };
+  return (
+    <VStack className="w-100%">
+      <HStack className="relative justify-center h-[7rem] md:h-[10rem]">
+        <h1 className="text-[5rem] md:text-[10rem] font-[500] space-x-4 opacity-5 left-[1rem] top-[50%] leading-[1] translate-y-[-60%] absolute">
+          Students
+        </h1>
+        <h1 className="section-heading text-center">Testimonials</h1>
+      </HStack>
+      <Center className="w-[100%]">
+        <Swiper {...swiperParams}>
+          {[...Array(4)].map((_, index) => (
+            <SwiperSlide>
+              <div className="grid sm:grid-cols-[1fr_1.3fr] sm:gap-[1rem] gap-[0.5rem] md:gap-[3rem] w-[100%] common-space-x">
+                <div className="w-[100%] rounded-lg overflow-hidden">
+                  <iframe
+                    width="100%"
+                    height="100%"
+                    src="https://www.youtube.com/embed/xXqgcm8dWyE?si=a-JgGuCV4C9qbK1a"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen
+                  ></iframe>
+                </div>
+                <div className="w-100% relative overflow-hidden">
+                  <VStack className="p-[2rem] border-[2px] border-black rounded-lg gap-4">
+                    <p className="text-basic">
+                      Dopa's guidance not only instilled the belief that I can
+                      become a doctor but also emphasized the importance of
+                      studying smartly, highlighting that working intelligently
+                      is key to success—a crucial lesson in pursuing my dream.
+                      Thank you DOPA {index}
+                    </p>
+                    <hr className="border-black" />
+                    <HStack className="w-[100%] justify-between">
+                      <VStack>
+                        <p className="text-basic font-[600]">Diya</p>
+                        <p className="text-small">Alumni</p>
+                      </VStack>
+                      <HStack className="gap-2">
+                        <div className="aspect-square testimonial-custom-prev transition-[all_0.3s_ease] hover:bg-black hover:text-white cursor-pointer p-[0.3rem] rotate-[180deg] border-[2px] border-black text-[1.5rem] rounded-full">
+                          <MdOutlineArrowForward />
+                        </div>
+                        <div className="aspect-square testimonial-custom-next transition-[all_0.3s_ease] hover:bg-black hover:text-white cursor-pointer p-[0.3rem] border-[2px] border-black text-[1.5rem] rounded-full">
+                          <MdOutlineArrowForward />
+                        </div>
+                      </HStack>
+                    </HStack>
+                  </VStack>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </Center>
+    </VStack>
+  );
+}
--- src/pages/dopa-thrissur/index.jsx
+++ src/pages/dopa-thrissur/index.jsx
@@ -2,14 +2,22 @@ import VStack from "@/Components/BasicComponents/VStack/VStack";
 import MainLayout from "@/Layouts/MainLayout";
 import CoursesOffered from "@/PageComponents/CoursesOffered/CoursesOffered";
 import AdmissionSection from "@/PageComponents/DopaThrissurLanding/AdmissionSection/AdmissionSection";
+import CrackNeetSection from "@/PageComponents/DopaThrissurLanding/CrackNeetSection/CrackNeetSection";
+import FacilitiesSection from "@/PageComponents/DopaThrissurLanding/FacilitiesSection/FacilitiesSection";
+import StudentTestimonials from "@/PageComponents/DopaThrissurLanding/StudentTestimonials/StudentTestimonials";
 import React from "react";
 
 export default function index() {
   return (
     <MainLayout>
-      <VStack className="min-h-[100vh] pt-[7rem]">
-        <AdmissionSection />
-        <CoursesOffered />
+      <VStack className="pt-[7rem] gap-[3rem]">
+        <StudentTestimonials />
+        <div>
+          <AdmissionSection />
+          <FacilitiesSection />
+        </div>
+        <CrackNeetSection />
+        {/* <CoursesOffered /> */}
       </VStack>
     </MainLayout>
   );
