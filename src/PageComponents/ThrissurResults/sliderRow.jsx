99707d3d4351ba9da37596f46c54a245d6952d37
rishadAlamy <rishadpt16@gmail.com>
2024-03-10 10:38:01 +0530

    result finished

--- src/PageComponents/ThrissurResults/sliderRow.jsx
+++ src/PageComponents/ThrissurResults/sliderRow.jsx
@@ -1,34 +1,58 @@
+// SliderRow.jsx
 import React from "react";
-
 import HStack from "@/Components/BasicComponents/HStack/HStack";
 import Image from "next/image";
-export default function SliderRow() {
+
+export default function SliderRow({ direction }) {
+ 
+console.log(direction,"direction")
+
   return (
-        <HStack >
-          <div className="relative w-[40rem]  aspect-[5/1] ">
-            <Image
-              src="/Assets/thrissurLanding/results/thrissur_result_row1.png"
-              alt="dopa_thrissur_results"
-              fill
-            />
-          </div>
+    <HStack
+      style={{
+        animation: `${direction === 1 ? 'slide-left' : 'slide-right'} 3s infinite`, // Adjust the duration as needed
+      }}
+      className="min-w-[100vw] overflow-hidden"
+    >
+      <div className="relative w-[40rem] aspect-[6/1] md:aspect-[5/3] overflow-hidden">
+        <Image
+          src="/Assets/thrissurLanding/results/thrissur_result_row1.png"
+          alt="dopa_thrissur_results"
+          fill
+        />
+      </div>
 
-          <div className="relative  w-[40rem] aspect-[5/1] ">
-            <Image
-              src="/Assets/thrissurLanding/results/thrissur_result_row1_2.png"
-              alt="dopa_thrissur_results"
-              fill
-            />
-          </div>
+      <div className="relative w-[40rem] aspect-[6/1] md:aspect-[5/3] overflow-hidden">
+        <Image
+          src="/Assets/thrissurLanding/results/thrissur_result_row1_2.png"
+          alt="dopa_thrissur_results"
+          fill
+        />
+      </div>
 
-          <div className="relative  w-[40rem] aspect-[5/1] ">
-            <Image
-              src="/Assets/thrissurLanding/results/thrissur_result_row1_2.png"
-              alt="dopa_thrissur_results"
-              fill
-            />
-          </div>
-          
-        </HStack>
+      <div className="relative w-[40rem] aspect-[6/1] md:aspect-[5/3] overflow-hidden">
+        <Image
+          src="/Assets/thrissurLanding/results/thrissur_result_row1_2.png"
+          alt="dopa_thrissur_results"
+          fill
+        />
+      </div>
+
+      <div className="relative w-[40rem] aspect-[6/1] md:aspect-[5/3] overflow-hidden">
+        <Image
+          src="/Assets/thrissurLanding/results/thrissur_result_row1_2.png"
+          alt="dopa_thrissur_results"
+          fill
+        />
+      </div>
+
+      <div className="relative w-[40rem] aspect-[6/1] md:aspect-[5/3] overflow-hidden">
+        <Image
+          src="/Assets/thrissurLanding/results/thrissur_result_row1_2.png"
+          alt="dopa_thrissur_results"
+          fill
+        />
+      </div>
+    </HStack>
   );
 }
