40101bd5180dcef01ce6d6137a755045949b5daa
Muhsin Neyyathur <muhsinny333@gmail.com>
2024-03-10 10:46:51 +0530

    feat: Working on the gallery

---
+++ src/PageComponents/DopaThrissurLanding/Gallery/Gallery.jsx
@@ -0,0 +1,16 @@
+import HStack from '@/Components/BasicComponents/HStack/HStack'
+import VStack from '@/Components/BasicComponents/VStack/VStack'
+import React from 'react'
+
+export default function Gallery() {
+  return (
+    <VStack>
+        <HStack>
+            <h1 className='section-heading'>Gallery</h1>
+        </HStack>
+        <div className='grid'>
+
+        </div>
+    </VStack>
+  )
+}
