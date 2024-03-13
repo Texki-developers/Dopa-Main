c9748728014a2c0b3c054c1a888c74b8f1522e71
rishadpt <rishadpt16@gmail.com>
2024-03-13 18:16:43 +0530

    fix: issues

--- src/Components/Form/TrendingForm/trendingForm.data.js
+++ src/Components/Form/TrendingForm/trendingForm.data.js
@@ -37,3 +37,57 @@ export const classess = [
     option: "Other",
   },
 ];
+
+
+export const Enquiryclassess = [
+  {
+    id: uuidv4(),
+    value: 8,
+    option: "8th",
+  },
+  {
+    id: uuidv4(),
+    value: 'repeaterResidential',
+    option: "Repeaters Residential",
+  },
+  {
+    id: uuidv4(),
+    value: 'repeatersOnline',
+    option: "Repeaters Online",
+  },
+  {
+    id: uuidv4(),
+    value: 'repeatersOffline',
+    option: "Repeaters Offline",
+  },
+  {
+    id: uuidv4(),
+    value: 'neetcrash',
+    option: "Neet Crash",
+  },
+  {
+    id: uuidv4(),
+    value: 9,
+    option: "9th",
+  },
+  {
+    id: uuidv4(),
+    value: 10,
+    option: "10th",
+  },
+  {
+    id: uuidv4(),
+    value: 11,
+    option: "11th",
+  },
+  {
+    id: uuidv4(),
+    value: 12,
+    option: "12th",
+  },
+  {
+    id: uuidv4(),
+    value: 'other',
+    option: "Other",
+  },
+];
