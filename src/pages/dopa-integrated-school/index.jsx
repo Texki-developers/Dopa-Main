e0b7d6892984f9a93765f67133c8142e72ac17e7
rishadpt <rishadpt16@gmail.com>
2024-03-17 15:52:45 +0530

    fix: conflict

--- src/pages/dopa-integrated-school/index.jsx
+++ src/pages/dopa-integrated-school/index.jsx
@@ -2,6 +2,8 @@ import VStack from "@/Components/BasicComponents/VStack/VStack";
 import AboutDopa from "@/PageComponents/DopaIntegratedSchool/AboutDopa/AboutDopa";
 import IntegratedTestimonials from "@/PageComponents/DopaIntegratedSchool/IntegratedTestimonials/IntegratedTestimonials";
 import JoinDopaSection from "@/PageComponents/DopaIntegratedSchool/JoinDopaSection/JoinDopaSection";
+import EnrollLayout from "@/PageComponents/DopaIntegratedSchool/Enroll/EnrollLayout";
+import SuccessStories from "@/PageComponents/DopaIntegratedSchool/successStories/SuccessStories";
 import React from "react";
 
 export default function DopaIntegratedSchool() {
@@ -10,6 +12,8 @@ export default function DopaIntegratedSchool() {
       <AboutDopa />
       <IntegratedTestimonials />
       <JoinDopaSection />
+      <SuccessStories/>
+      <EnrollLayout/>
     </VStack>
   );
 }
