fc8f46816c2ce2687206df332256262fc3b4c77c
Muhsin Neyyathur <muhsinny333@gmail.com>
2024-08-20 01:02:11 +0530

    fix: changes

--- src/pages/about-dopa/index.jsx
+++ src/pages/about-dopa/index.jsx
@@ -1,5 +1,6 @@
 import MainLayout from "@/Layouts/MainLayout";
 import CustomizableBanner from "@/PageComponents/banner/CustomizableBanner";
+import Image from "next/image";
 import React from "react";
 
 export default function index() {
@@ -8,8 +9,11 @@ export default function index() {
       <CustomizableBanner
         title={["About Us"]}
         description="DOPA is an initiative started by a group of young doctors who have completed MBBS from Calicut MedicalCollege, Kerala. We support passionate students to crack their entrance exam. Scholarships will be offered tostudents achieving outstandingscores in the Integrated School Entrance Exam!"
+        rtl
       >
-        
+        <div className="w-[100%] aspect-[741/400] relative mt-[2rem]">
+          <Image src="/Assets/homeV2/banner_dopa_directors.png" fill />
+        </div>
       </CustomizableBanner>
     </MainLayout>
   );
--- src/pages/dopa-contact/index.jsx
+++ src/pages/dopa-contact/index.jsx
@@ -5,9 +5,61 @@ import PrimaryInput from "@/Components/InputFields/PrimaryInput/PrimaryInput";
 import MainLayout from "@/Layouts/MainLayout";
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useToast } from "@chakra-ui/react";
+import { saveForms } from "@/utils/Services/trending.service";
 
 export default function index() {
+  const [isLoading, setLoading] = useState(false);
+  const [showFeedback, setshowFeedback] = useState(false);
+  const toast = useToast();
+  const {
+    register,
+    formState: { errors },
+    reset,
+    handleSubmit,
+  } = useForm({ mode: "onChange" });
+
+  const onFormSubmission = async (data) => {
+    console.log(data);
+    setLoading(true);
+    data.type = "Contact Form";
+    const response = await saveForms(data);
+    if (response.status === 200) {
+      toast({
+        status: "success",
+        description: "Registered Successfully!",
+        title: "Great!",
+      });
+      setLoading(false);
+      reset({
+        email: "",
+        name: "",
+        class: "",
+        school: "",
+        whatsapp: "",
+        place: "",
+      });
+    } else {
+      setLoading(false);
+      toast({
+        status: "error",
+        description: "Something went wrong!",
+        title: "Ugh no!",
+      });
+    }
+    setLoading(false);
+    setshowFeedback(true);
+    reset({
+      email: "",
+      name: "",
+      class: "",
+      school: "",
+      whatsapp: "",
+      place: "",
+    });
+  };
   return (
     <MainLayout>
       <Center className="pt-[5.5rem] md:pt-[7rem] pb-[2rem]">
@@ -81,11 +133,51 @@ export default function index() {
           <div className="flex justify-center items-center">
             <div className="p-[2rem] rounded-[14px] min-w-[100%] sm:min-w-[80%] max-w-[30rem] justify-center items-center border-[2px] border-[#000] h-max">
               <VStack className="gap-[0.5rem]">
-                <PrimaryInput color="#323232" isLine label="Your Name" />
-                <PrimaryInput color="#323232" isLine label="District" />
-                <PrimaryInput color="#323232" isLine label="Phone Number" />
-                <PrimaryInput color="#323232" isLine label="Class" />
-                <Button className="btn-solid mt-[1rem] bg-primary-500 text-white font-[600] border-[1px] shadow-[0px_3.42px_3.42px_0px_#00000040]">
+                <PrimaryInput
+                  color="#323232"
+                  isLine
+                  label="Your Name"
+                  register={register("name", {
+                    required: "Name field is required",
+                  })}
+                  error={errors?.name?.message}
+                  required
+                />
+                <PrimaryInput
+                  color="#323232"
+                  isLine
+                  label="District"
+                  register={register("place", {
+                    required: "Your Place is required",
+                  })}
+                  required
+                  error={errors?.place?.message}
+                />
+                <PrimaryInput
+                  color="#323232"
+                  isLine
+                  label="Phone Number"
+                  register={register("whatsapp", {
+                    required: "whatsapp field is required",
+                  })}
+                  required
+                  error={errors?.whatsapp?.message}
+                />
+                <PrimaryInput
+                  color="#323232"
+                  isLine
+                  label="Class"
+                  register={register("class", {
+                    required: "class Field is required",
+                  })}
+                  error={errors?.class?.message}
+                  required
+                />
+                <Button
+                  onClick={handleSubmit(onFormSubmission)}
+                  isLoading={isLoading}
+                  className="btn-solid mt-[1rem] bg-primary-500 text-white font-[600] border-[1px] shadow-[0px_3.42px_3.42px_0px_#00000040]"
+                >
                   Contact Us
                 </Button>
               </VStack>
