220f8eb83f697e960afafdc4fcda0e0a9cfe7725
rishadpt <rishadpt16@gmail.com>
2025-05-08 11:47:21 +0530

    banner updated in rpeaters

--- src/PageComponents/DopaRepeaters/Banner/Banner.jsx
+++ src/PageComponents/DopaRepeaters/Banner/Banner.jsx
@@ -10,7 +10,7 @@ export default function Banner() {
     <Center className="w-[100%]">
       <div className="grid common-space-x gap-[1rem] md:gap-[6rem] grid-cols-1 md:grid-cols-[1fr_1fr] xl:grid-cols-[1.5fr_1fr] w-[100%]">
         <VStack className="gap-[1rem]">
-          <HStack className="gap-[1rem] w-[100%] justify-center md:justify-start">
+          {/* <HStack className="gap-[1rem] w-[100%] justify-center md:justify-start">
             <div className="aspect-square w-[5rem] sm:w-[6rem]  xl:w-[10rem] relative">
               <Image src="/Assets/repeaters/neet-heading.png" fill />
             </div>
@@ -18,9 +18,9 @@ export default function Banner() {
               Repeaters <br />
               Program
             </h1>
-          </HStack>
-          <div className="aspect-[314/187] w-[80%] mx-auto md:w-[100%] relative">
-            <Image src="/Assets/repeaters/banner-img.png" fill />
+          </HStack> */}
+          <div className="aspect-[322/261] w-[80%] mx-auto md:w-[100%] relative">
+            <Image src="/Assets/repeaters/banner-img.jpg" fill />
           </div>
           <div className="w-[100%] grid grid-cols-3 p-[7px] text-[0.9rem] sm:text-[1rem] mx-auto rounded-md text-center uppercase card-heading text-primary-600 bg-[#F8F5A9] max-w-[40rem]">
             <p className="w-[100%] after:content-[''] after:h-[100%] after:w-[1px] after:bg-primary-600 after:block relative after:right-0 after:top-0 after:absolute">
